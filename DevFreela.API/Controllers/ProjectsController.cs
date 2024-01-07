3127c4534b24ed736610602a584e03249567ad70
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-07 20:38:45 -0300

    init

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+
+[ApiController]
+[Route("api/projects")]
+public class ProjectsController : ControllerBase
+{
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+
+}
