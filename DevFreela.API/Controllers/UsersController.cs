f9765925934734b98f2fab8d058cefd115c9876c
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-08 06:55:08 -0300

    feat:endpoints de criação de usuario

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers;
+[Route("api/users")]
+public class UsersController : Controller
+{
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateUserModel createUserModel)
+    {
+        return Created();
+    }
+
+    [HttpPut("{id}/login")]
+    public IActionResult Login(int id, [FromBody] LoginModel login)
+    {
+        return NoContent(); //TODO Retornar token JWT.
+    }
+}
