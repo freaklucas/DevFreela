52abbe23d1978411fce14e1bf4a7731e7b5e1af8
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-07 21:21:29 -0300

    feat:testando as rotas

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,14 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers;
 
 [ApiController]
 [Route("api/projects")]
+
 public class ProjectsController : ControllerBase
 {
-    public IActionResult Get()
+    [HttpGet]
+    public IActionResult Get(string query)
     {
         return Ok();
     }
 
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateProjectModel createProject)
+    {
+        if (createProject.Title.Length > 50)
+        {
+            return BadRequest();
+        }
+
+        return Created();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+    {
+        if (updateProject.Description.Length > 250)
+        {
+            return BadRequest();
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
 }
