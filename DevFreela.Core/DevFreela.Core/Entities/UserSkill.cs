a26f96cd92b84a49707540e731b5bdd0f4d69e95
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-09 22:30:18 -0300

    feat:entidades elementares

---
+++ DevFreela.Core/DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,12 @@
+namespace DevFreela.Core.Entities;
+
+public class UserSkill : BaseEntity
+{
+    public UserSkill(int idUser, int idSkill)
+    {
+        IdUser = idUser;
+        IdSkill = idSkill;
+    }
+    public int IdUser { get; private set; }
+    public int IdSkill { get; private set; }
+}
---
+++ DevFreela.Core/DevFreela.Core/Enums/ProjectStatusEnum.cs
@@ -0,0 +1,6 @@
+namespace DevFreela.Core.Enums;
+
+public enum ProjectStatusEnum
+{
+    
+}
