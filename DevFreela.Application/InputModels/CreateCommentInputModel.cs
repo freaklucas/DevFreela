4976712552dbad45280034f628ef7ddc2f3ab99d
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-13 12:41:31 -0300

    feat:modelo create,update para Projectservice

--- DevFreela.Application/InputModels/CreateCommentInputModel.cs
+++ DevFreela.Application/InputModels/CreateCommentInputModel.cs
@@ -2,4 +2,7 @@
 
 public class CreateCommentInputModel
 {
+    public string Content { get; set; }
+    public int IdProject { get; set; }
+    public int IdUser { get; set; }
 }
--- DevFreela.Application/InputModels/UpdateProjectInputModel.cs
+++ DevFreela.Application/InputModels/UpdateProjectInputModel.cs
@@ -1,6 +1,25 @@
-﻿namespace DevFreela.Application.InputModels;
+﻿using DevFreela.Core.Enums;
+
+namespace DevFreela.Application.InputModels;
 
 public class UpdateProjectInputModel
 {
+    public UpdateProjectInputModel(
+        string title, 
+        string description, 
+        int idClient, 
+        int idFreelancer, 
+        decimal totalCost, 
+        DateTime finishedAt
+        )
+    {
+        Title = title;
+        Description = description;
+        TotalCost = totalCost;
+    }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal TotalCost { get; set; }
 }
 
