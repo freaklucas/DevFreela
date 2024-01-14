070d314cfa2de41023cde2eb0fcef4b234ba8fb8
freaklucas <lucas.oliveira.freak@gmail.com>
2024-01-14 15:40:48 -0300

    feat:id em project e skillService

---
+++ DevFreela.Application/Services/Interfaces/ISkillService.cs
@@ -0,0 +1,9 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Entities;
+
+namespace DevFreela.Application.Services.Interfaces;
+
+public interface ISkillService
+{
+    List<SkillViewModel> GetAll();
+}
